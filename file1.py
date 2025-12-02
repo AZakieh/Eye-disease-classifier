560f11df9906860c99011fbf1d8d12e3a1981842
Adam Zakieh <adam.zakieh23@gmail.com>
2025-12-02 17:00:49 +0000

    added eyeresnet class, and started coding the training process

---
+++ file1.py
@@ -0,0 +1,105 @@
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import pandas as pd
+from skimage import io, transform
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils, models
+
+# Ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+plt.ion()
+
+class EyeLandmarksDataset(Dataset):
+    """ Eye landmarks dataset """
+
+    def __init__(self, csv_file=None, root_dir=None, tarnsform=None):
+        """
+        Arg:
+            csv_file (string): path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.landmarks_frame = 100 #pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        # for now returning that we have 100 eye image scans
+        return self.landmarks_frame #len(self.landmarks_frame)#
+    
+    def __getitem__(self, idx):
+        # creating a fake eye image
+        # torch.randn generates numbers in a normal distribution
+
+        fake_image = torch.randn(3, 224, 224)
+
+        fake_label = torch.randint(0, 2, (1,)).item()
+
+        return fake_image, fake_label
+        
+
+class EyeResNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = models.resnet18(weights="DEFAULT")
+        self.model.fc = nn.Linear(in_features=512, out_features=2)
+        
+    
+    def forward(self, x):
+        result = self.model(x)
+        return result
+
+
+dataset = EyeLandmarksDataset()
+
+
+
+train_loader = DataLoader(dataset, batch_size=32, shuffle=True, num_workers=0)
+images_batch, labels_batch = next(iter(train_loader))
+model = models.resnet18(weights="DEFAULT")
+model.fc = nn.Linear(in_features=512, out_features=2)
+outputs = model(images_batch)
+print(outputs.shape)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+
+
+for epoch in range(2):
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        images_batch, labels_batch = data
+
+        optimizer.zero_grad()
+
+        outputs = model(images_batch)
+        loss = criterion(outputs, labels_batch)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if i % 1 == 0:
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 1:.3f}')
+            running_loss = 0.0
+print("finished training")
+
+
+
+
+
+
+
+
+
+
+#model = torch.compile(net)
+#with torch.amp.autocast("cuda"):
+ #   output = model(x)
