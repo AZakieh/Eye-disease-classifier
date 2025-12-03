1c4658d47060e62d382a93f3a695dce5f20802b7
Adam Zakieh <adam.zakieh23@gmail.com>
2025-12-03 20:15:10 +0000

    Wrote the upload image function, which loads the file explorer, shows only images and sends it to the ai model to predict whether the eye is healthy or not

--- gui.py
+++ gui.py
@@ -1,12 +1,46 @@
+import eye_classifier
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt6.QtWidgets import( 
+QApplication, QMainWindow, QWidget, 
+QVBoxLayout, QLabel, QPushButton, QFileDialog)
 
 
 class EyeApp(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.model = eye_classifier.load_model_for_inference()
         self.setWindowTitle("Eye Disease Classifier")
         self.resize(500, 500)
+        
+        layout = QVBoxLayout()
+        button = QPushButton("Upload Image")
+        button.clicked.connect(self.upload_image)
+        layout.addWidget(button)
+        self.label = QLabel("No image selected")
+        layout.addWidget(self.label)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+    def upload_image(self):
+        self.dialog = QFileDialog(self)
+        self.dialog.setFileMode(QFileDialog.FileMode.ExistingFiles)
+        self.dialog.setNameFilter("Images (*.png *.jpg)")
+        self.dialog.setViewMode(QFileDialog.ViewMode.Detail)
+        self.dialog.setDirectory('C:/images/')
+        if self.dialog.exec():
+            fileNames = self.dialog.selectedFiles()
+            file_path = fileNames[0]
+            result = eye_classifier.predict_image(self.model, file_path)
+            self.label.setText(f"Prediction: {result}")
+            
+    
+
+
+
+
+
 
 
 app = QApplication(sys.argv)
